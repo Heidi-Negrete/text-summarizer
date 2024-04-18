5b7804280740b52b9abc24dd80829c4dbccf585d
Heidi Negrete <heidi@negrete.dev>
2024-04-17 20:29:46 -0400

    add cors middleware and list of allowed origins for local development

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from backend import ai_summarize
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class Text(BaseModel):
@@ -8,6 +9,20 @@ class Text(BaseModel):
 
 
 app = FastAPI()
+
+origins = [
+    # Allowed origins here
+    "http://localhost",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
@@ -17,6 +32,5 @@ def read_root():
 
 @app.post("/summarize")
 async def summarize(text: Text):
-    print('ok hitting endpoint.')
     summarized = await ai_summarize.summarize_text(text.text)
     return {"content": summarized}
