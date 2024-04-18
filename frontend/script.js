f5e4be2a1afc98e24124984529c9436a00ab2f47
Heidi Negrete <heidi@negrete.dev>
2024-04-17 20:31:29 -0400

    frontend client fetches data from backend w basic error handling

--- frontend/script.js
+++ frontend/script.js
@@ -7,6 +7,7 @@ submitButton.addEventListener("click", submitData);
 
 submitButton.disabled = true
 
+
 function verifyTextLength(event) {
     const textarea = event.target;
 
@@ -22,4 +23,35 @@ function submitData(event) {
     submitButton.classList.add("submit_button--loading");
     
     const text_to_summarize = textArea.value;
-}
+
+
+    const url = "http://127.0.0.1:8000"
+    //const secret = ""
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    //myHeaders.append("Authorization", `Bearer ${secret}`);
+
+    const raw = JSON.stringify({
+    "text": text_to_summarize
+    });
+
+    const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+    };
+
+    fetch(`${url}/summarize`, requestOptions)
+    .then(response => response.json())
+        .then(text => {
+            summarizedTextArea.value = text.content;
+        })
+    .catch((error) => {
+        console.error(error);
+        summarizedTextArea.value = "Something went wrong. Please try again later.";
+    })
+    .finally( () => submitButton.classList.remove("submit_button--loading"));
+
+  }
